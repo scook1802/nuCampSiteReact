87b6dc071a96ba93fec63b16e9aaff81a6dbd112
Samantha Miller <scook1802@gmail.com>
2022-11-03 12:46:01 -0400

    Router Setup

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,8 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
-}
+export const selectCampsiteById = (id) => {
+    return (
+        CAMPSITES.find((campsite) => campsite.id === id)
+    );
+};
