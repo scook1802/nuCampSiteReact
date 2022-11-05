76d97ac509a06a5817111566d42fd164fa567c1c
Samantha Miller <scook1802@gmail.com>
2022-11-05 11:26:05 -0400

    Workshop2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 
 function App() {
@@ -27,6 +28,10 @@ function App() {
         <Route 
           path='directory/:campsiteId' 
           element={<CampsiteDetailPage />} 
+        />
+        <Route 
+          path='about'
+          element={<AboutPage />}
         />
       </Routes>
       <Footer />
