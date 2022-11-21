12f59548fe1da90068e965e6db62900f6ff4b09f
Samantha Miller <scook1802@gmail.com>
2022-11-20 21:34:10 -0500

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,20 +1,24 @@
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { validateCommentForm } from '../../utils/validateCommentForm';
+import { addComment } from './commentsSlice';
 
 
 const CommentForm = ({ campsiteId }) => {
     const [modalOpen, setModalOpen] = useState(false);
-
+    const dispatch =useDispatch();
     const handleSubmit = (values) => {
         const comment = {
             campsiteId: parseInt(campsiteId),
             rating: values.rating,
             author: values.author,
-            text: values.commentText
+            text: values.commentText,
+            date: new Date(Date.now()).toISOString()
         };
         console.log(comment);
+        dispatch(addComment(comment));
         setModalOpen(false);
     };
     return (
